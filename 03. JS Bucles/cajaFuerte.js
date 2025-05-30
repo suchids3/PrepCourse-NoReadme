964fc75532aa078d7cfb948841cc5564cc75f6cf
Melanie Cabral <ayelencabral585@gmail.com>
2025-05-30 15:12:46 -0300

    clase sobre bucles

--- 03. JS Bucles/cajaFuerte.js
+++ 03. JS Bucles/cajaFuerte.js
@@ -1,15 +1,29 @@
 function cajaFuerte(codigoSecreto, cantidadIntentos){
   /* TU CODIGO */
-  if (codigoSecreto.legth != 4) return "El codigo debe tener exactamente 4 digitos";
+  if (codigoSecreto.length != 4) return "El codigo debe tener exactamente 4 digitos";
   
-  for (let i = 0; i < codigoSecreto.legth; i++) {
-    if (isNaN(codigoSecreto[i])) return "El codigo secreto solo puede estar conformado por numeros"
+  for (let i = 0; i < codigoSecreto.length; i++) {
+    if (isNaN(codigoSecreto[i])) return "El codigo secreto solo puede estar conformado por numeros";
   }
+
+  if (validarNumerosRepetidos(codigoSecreto)) {
+    return "el codigo secreto no puede tener numeros repetidos";
+  }
+
+  if (cantidadIntentos < 1 || cantidadIntentos > 5) {
+    return "Solo se permite una cantidad de intentos mayor a 0 y menor a 6";
+  }
+
+  return codigoSecreto.toString() + cantidadIntentos.toString();
 }
 
-function validarNumerosRepetidos(codigo){
+function validarNumerosRepetidos(codigo) {
   /* TU CODIGO */
-  
+  for (var i = 0; i < codigo.length - 1; i++) {
+    for (var j = i + 1; j < codigo.length; j++) {
+      if (codigo[i] == codigo[j]) return true;
+    }
+  }
 }
 
 // <------- Contador de intentos -----> no modificar
@@ -17,7 +31,33 @@ var contadorIntentos = 1
 
 function desbloquearCajaFuerte(codigoSecreto, cantidadIntentos, codigoDesbloqueo){
   /* TU CODIGO */
-  
+  if (codigoDesbloqueo.length != 4) return "El codigo debe tener exactamente 4 digitos"; 
+
+  for (let i = 0; i < codigoDesbloqueo.length; i++) {
+    if (isNaN(codigoDesbloqueo[i])) 
+      return "El codigo de desbloqueo solo puede estar conformado por numeros";
+  }
+
+  if (validarNumerosRepetidos(codigoDesbloqueo)) 
+    return "el codigo no puede tener numeros repetidos";
+
+  if (codigoSecreto == codigoDesbloqueo) {
+    return "Acceso concedido despues de : " + contadorIntentos + " intentos";
+  }
+
+  switch (true) {
+    case codigoDesbloqueo % 2 == 0:
+      console.log ("el codigo es dividible x 2");
+      break;
+    case codigoDesbloqueo > codigoSecreto:
+      console.log ("codigo incorrecto demasiado alto");
+      break;
+    default:
+      console.log("codigo incorrecto");
+      break;
+  }
+  contadorIntentos++;
+  if (contadorIntentos > cantidadIntentos) return "Acceso denegado. Se agotaron los intentos."
 }
 
 // <------- NO TOCAR -------->
@@ -25,4 +65,4 @@ module.exports = {
   cajaFuerte,
   desbloquearCajaFuerte,
   validarNumerosRepetidos
-}
+};
--- build/public/bundle.js
+++ build/public/bundle.js
@@ -81,16 +81,32 @@ module.exports = {
 
 function cajaFuerte(codigoSecreto, cantidadIntentos){
   /* TU CODIGO */
-  if (codigoSecreto.legth != 4) return "El codigo debe tener exactamente 4 digitos";
+  if (codigoSecreto.length != 4) return "El codigo debe tener exactamente 4 digitos";
   
-  for (let i = 0; i < codigoSecreto.legth; i++) {
-    if (isNaN(codigoSecreto[i])) return "El codigo secreto solo puede estar conformado por numeros"
+  for (let i = 0; i < codigoSecreto.length; i++) {
+    if (isNaN(codigoSecreto[i])) return "El codigo secreto solo puede estar conformado por numeros";
   }
+
+  if (validarNumerosRepetidos(codigoSecreto)) {
+    return "el codigo secreto no puede tener numeros repetidos";
+  }
+
+  if (cantidadIntentos < 1 || cantidadIntentos > 5) {
+    return "Solo se permite una cantidad de intentos mayor a 0 y menor a 6";
+  }
+
+  return codigoSecreto.toString() + cantidadIntentos.toString();
 }
 
 function validarNumerosRepetidos(codigo){
   /* TU CODIGO */
-  
+  //for (var i = 0; i < codigo.length; i++) {
+    //for (var j = 0; j < codigo.length; j++) {
+      //if (codigo[i] == codigo[j]) return true;
+    //}
+  //}
+  var setDeNumeros = new Set (codigo);
+  return setDeNumeros.size != codigo.length;
 }
 
 // <------- Contador de intentos -----> no modificar
@@ -98,7 +114,33 @@ var contadorIntentos = 1
 
 function desbloquearCajaFuerte(codigoSecreto, cantidadIntentos, codigoDesbloqueo){
   /* TU CODIGO */
-  
+  if (codigoDesbloqueo.length != 4) return "El codigo debe tener exactamente 4 digitos"; 
+
+  for (let i = 0; i < codigoDesbloqueo.length; i++) {
+    if (isNaN(codigoDesbloqueo[i])) 
+      return "El codigo de desbloqueo solo puede estar conformado por numeros";
+  }
+
+  if (validarNumerosRepetidos(codigoDesbloqueo)) 
+    return "el codigo no puede tener numeros repetidos";
+
+  if (codigoSecreto == codigoDesbloqueo) {
+    return "Acceso concedido despues de : " + contadorIntentos + " intentos";
+  }
+
+  switch (true) {
+    case codigoDesbloqueo % 2 == 0:
+      console.log ("el codigo es dividible x 2");
+      break;
+    case codigoDesbloqueo > codigoSecreto:
+      console.log ("codigo incorrecto demasiado alto");
+      break;
+    default:
+      console.log("codigo incorrecto");
+      break;
+  }
+  contadorIntentos++;
+  if (contadorIntentos > cantidadIntentos) return "Acceso denegado. Se agotaron los intentos."
 }
 
 // <------- NO TOCAR -------->
@@ -106,7 +148,7 @@ module.exports = {
   cajaFuerte,
   desbloquearCajaFuerte,
   validarNumerosRepetidos
-}
+};
 
 /***/ }),
 
