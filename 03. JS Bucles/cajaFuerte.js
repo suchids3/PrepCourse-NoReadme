13ce39f015d507cad41447749385bd7fc819226f
Melanie Cabral <ayelencabral585@gmail.com>
2025-06-02 12:16:42 -0300

    "Proyecto finalizado"

--- 03. JS Bucles/cajaFuerte.js
+++ 03. JS Bucles/cajaFuerte.js
@@ -66,3 +66,4 @@ module.exports = {
   desbloquearCajaFuerte,
   validarNumerosRepetidos
 };
+
--- build/public/bundle.js
+++ build/public/bundle.js
@@ -148,6 +148,8 @@ module.exports = {
   validarNumerosRepetidos
 };
 
+
+
 /***/ }),
 
 /***/ "../04. JS Arrays/monitoreoActividad.js":
@@ -242,21 +244,66 @@ const { perfiles } = __webpack_require__(/*! ../build/js/perfiles.js */ "./js/pe
 var asistente = {
     verPerfiles: function(opcion){
         /* TU CODIGO */
+      switch (opcion) {
+        case "todo":
+           return perfiles;
+        case "nombre":
+           return perfiles.map(function (perfil) {
+            return perfil.usuario;
+           });
+        case "codigo":
+           return perfiles.map(function (perfil) {
+            return perfil.codigo;
+           });
+        case "nivel":
+           return perfiles.map(function (perfil) {
+            return perfil.nivel_de_autorizacion;
+           });
+        case "antiguedad":
+           return perfiles.map(function (perfil) {
+            return perfil.antiguedad;
+           });
+        }
       
     },
     
     verPerfilesPorAntiguedad: function(){
         /* TU CODIGO */
-        
+        const copiaPerfiles = perfiles.map (function (perfil) {
+            return perfil;
+        });
+
+        return copiaPerfiles.sort(function (a, b) {
+            return b.antiguedad - a.antiguedad;
+        });
     },
 
     verAdministradores: function(){
         /* TU CODIGO */
+        return perfiles.filter(function (perfil) {
+            return perfil.nivel_de_autorizacion == "admin";
+        });
 
     },
 
     modificarAcceso: function(usuario, codigo){
         /* TU CODIGO */
+        if (
+            !perfiles.some(function (perfil) {
+            return perfil.usuario == usuario;
+        })) {
+            return "usuario no encontrado"
+        }
+        if (codigo.length != 4) {
+                return "codigo de acceso invalido, debe contener solo 4 numeros";
+        }
+        const perfilEncontrado = perfiles.find (function (perfil) {
+            return perfil.usuario == usuario;
+        });
+
+        perfilEncontrado.codigo = codigo;
+        return "codigo de acceso modificado";
+
         
     }
 }
